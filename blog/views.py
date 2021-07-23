4e138f0c6174d09ded55339eae64f778c9eec03f
Dawid Dzhafarov <dawid.dzhafarov@gmail.com>
2021-07-23 17:41:09 +0200

    Views for posts created.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-# Create your views here.
+from django.views.generic import ListView, DetailView
+
+
 
 
 def home(request):
@@ -10,6 +12,17 @@ def home(request):
         'title': 'Home',
     }
     return render(request, 'blog/home.html', context,)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title':'About'})
